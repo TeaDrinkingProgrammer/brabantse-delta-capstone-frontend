e5b118e42c32c81cd63bdb176230220b00fe6e35
Teadrinkingprogrammer <teadrinkingprogrammer@proton.me>
2024-01-17 13:45:57 +0100

    Removed old code. Updated readme

--- src/pages/Home.py
+++ src/pages/Home.py
@@ -4,6 +4,7 @@ from dash import html
 dash.register_page(__name__, path='/')
 
 layout = html.Div([
-    html.H1('This is our Home page'),
-    html.Div('This is our Home page content.'),
+    html.H1('Brabantse Delta Sewage prediction'),
+    html.P('This is a demo-application that can predict the amount of sewage water that will be in the sewer-system in a specific area in Rucphen.'),
+    html.P('The predictions are quite accurate, but can miss some small peaks in rainfall.')
 ])
